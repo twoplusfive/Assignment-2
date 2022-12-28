9be36fcf10745e46564cd8a30a63ced6369c607b
twoplusfive <120657585+twoplusfive@users.noreply.github.com>
2022-12-28 22:11:09 +0530

    Update question4.c

--- question4.c
+++ question4.c
@@ -6,17 +6,15 @@
 #include<conio.h>
 int main()
 {
-    int num, n, r_num=0;
+    int num, n,p, r_num=0;
 
     printf("Enter a number: ");
     scanf("%d", &num);
     n = num;
     while(num!=0)
     {
-        r_num = r_num * 10;
-
-        r_num = num % 10 + r_num;
-
+        p=num%10;
+        r_num=(r_num*10)+p;
         num = num/10;
     }
     printf("Reversed Number of %d is %d\n",n, r_num);
